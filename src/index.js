be07450a5b81182426911e6c1a835ffa9e8963a2
Dejalma Arantes <dejalma.arantes>
2020-03-05 02:36:20 +0000

    Delete done

--- src/index.js
+++ src/index.js
@@ -69,6 +69,21 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
+app.delete('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const user = await User.findByIdAndDelete(_id)
+        
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (error) {
+        res.status(400).send()
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
@@ -126,6 +141,21 @@ app.patch('/tasks/:id', async (req, res) => {
         res.send(task)
     } catch (error) {
         res.status(400).send(error)
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const task = await Task.findByIdAndDelete(_id)
+        
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (error) {
+        res.status(400).send()
     }
 })
 
